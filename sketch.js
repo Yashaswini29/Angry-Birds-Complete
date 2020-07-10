d2b1c806df693c126da2fd9ef5e76b5d308e4641
Yashaswini29 <yashaswinihn.h.n@gmail.com>
2020-07-10 21:43:05 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -123,13 +123,13 @@ function keyPressed(){
 }
 
 async function getBackgroundImg(){
-    var response = await fetch("http://worldtimeapi.org/api/timezone/Asia/Kolkata");
+    var response = await fetch("https://worldtimeapi.org/api/timezone/Asia/Kolkata");
     var responseJSON = await response.json();
 
     var datetime = responseJSON.datetime;
     var hour = datetime.slice(11,13);
     
-    if(hour>=0600 && hour<=1900){
+    if(hour>=06 && hour<=19){
         bg = "sprites/bg1.png";
     }
     else{
